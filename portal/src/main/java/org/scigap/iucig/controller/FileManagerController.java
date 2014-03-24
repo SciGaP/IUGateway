dc8824fdffed07c3864b5d3811e096c501f6bba2
swsachith <swsachith@gmail.com>
2014-03-24 11:15:34 -0400

    added the filemanager controller

---
+++ portal/src/main/java/org/scigap/iucig/controller/FileManagerController.java
@@ -0,0 +1,32 @@
+package org.scigap.iucig.controller;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.scigap.iucig.filemanager.CommandExecutor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value="/filemanager/")
+public class FileManagerController {
+
+    private static CommandExecutor commandExecutor;
+
+    public FileManagerController() {
+        commandExecutor = new CommandExecutor();
+    }
+
+    /** Returns the result of a command using a hash map */
+    @ResponseBody
+    @RequestMapping(value="/command/{command}", method = RequestMethod.GET)
+    public String executeCommand(@PathVariable(value="command") final String command) {
+        commandExecutor.executeCommand(command);
+        return  commandExecutor.getResultMap().toString();
+    }
+
+}
