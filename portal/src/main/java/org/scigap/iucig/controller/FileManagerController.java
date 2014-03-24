81bac42e1ba3f6de608de1a4c8480087f962352e
swsachith <swsachith@gmail.com>
2014-03-24 11:50:36 -0400

    fixing a build error of the fileManagerController

--- portal/src/main/java/org/scigap/iucig/controller/FileManagerController.java
+++ portal/src/main/java/org/scigap/iucig/controller/FileManagerController.java
@@ -17,14 +17,13 @@ public class FileManagerController {
 
     private static CommandExecutor commandExecutor;
 
-    public FileManagerController() {
-        commandExecutor = new CommandExecutor();
-    }
-
     /** Returns the result of a command using a hash map */
     @ResponseBody
     @RequestMapping(value="/command/{command}", method = RequestMethod.GET)
     public String executeCommand(@PathVariable(value="command") final String command) {
+        if(commandExecutor == null) {
+            commandExecutor = new CommandExecutor();        
+        }
         commandExecutor.executeCommand(command);
         return  commandExecutor.getResultMap().toString();
     }
