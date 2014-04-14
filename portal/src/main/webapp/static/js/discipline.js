1bce548d5da99b49b67c38a532ea983e8718f60d
Chathuri Wimalasena <kamalasini@gmail.com>
2014-04-14 17:00:28 -0400

    getting existing user disciplines

--- portal/src/main/webapp/static/js/discipline.js
+++ portal/src/main/webapp/static/js/discipline.js
@@ -1,11 +1,12 @@
 var disciplineApp = angular.module("disciplineApp", ["user"]);
 
 disciplineApp.controller("DisciplineCtrl", function ($scope, $http) {
-//        $http({method: "GET", url: "getScienceDiscipline", cache: false}).
-        $http({method: "GET", url: "getUsersScienceDiscipline", cache: false}).
+        $http({method: "GET", url: "getScienceDiscipline", cache: false}).
             success(function (data, status) {
 //                $scope.disciplines = getDisciplines(data);
-                console.log(data);
+                var savedDiscipline = "{\"disciplines\":[{\"name\":\"Informatics\",\"id\":180},{\"name\":\"Early Childhood Education\",\"id\":107}]}";
+                $scope.disciplines = getUserDisciplines(data, savedDiscipline);
+                console.log($scope.disciplines);
 
             }).
             error(function (data, status) {
@@ -72,6 +73,45 @@ var getDisciplines = function (data) {
         disciplines.push(discipline);
     }
     return disciplines;
+}
+
+
+var getUserDisciplines = function (allDisciplines, savedDiscipline) {
+    savedDiscipline = JSON.parse(savedDiscipline);
+    var disciplines = [];
+    var discipline = {};
+    var subdisciplines = [];
+    var data1 = [];
+    data1 = savedDiscipline.disciplines;
+    for (var i in data1){
+        var subdiscipline = {};
+        subdiscipline.id = data1[i].id;
+        subdiscipline.name = data1[i].name;
+        discipline = getDisciplineContainsSubDiscipline(allDisciplines, subdiscipline.id);
+        discipline.subdisciplines = subdiscipline;
+        disciplines.push(discipline);
+    }
+    return disciplines;
+}
+
+var getDisciplineContainsSubDiscipline = function (allDisciplines, id){
+    for (var i in allDisciplines) {
+        var discipline = {};
+        var subdisciplines = [];
+        subdisciplines = allDisciplines[i].subdisciplines;
+        for (var j in subdisciplines){
+            var obj = subdisciplines[j];
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (key == id) {
+                        discipline.id =  allDisciplines[i].id;
+                        discipline.name = allDisciplines[i].name;
+                        return discipline;
+                    }
+                }
+            }
+        }
+    }
 }
 
 var getSubdisciplines  = function(id, disciplines){
