b9e3fd6842cb350da11a43a3e26fe4a9c6907b25
Chathuri Wimalasena <kamalasini@gmail.com>
2014-04-15 22:41:56 -0400

    adding select

--- portal/src/main/webapp/static/js/discipline.js
+++ portal/src/main/webapp/static/js/discipline.js
@@ -19,8 +19,8 @@ disciplineApp.controller("DisciplineCtrl", function ($scope, $http) {
                         if (selectedDisciplines != undefined && selectedDisciplines.length > 0){
 //                            $scope.item.primaryDisc = selectedDisciplines[0];
 //                            $scope.item.primarySubDisc = selectedDisciplines[0].subdisciplines;
-                            $scope.item.primaryDisc = allDisciplines[1];
-                            $scope.item.primarySubDisc = allDisciplines[1].subdisciplines;
+                            $scope.pDisc = allDisciplines[1];
+                            $scope.pSubDisc = allDisciplines[1].subdisciplines;
                             if (selectedDisciplines[1] != undefined){
                                 $scope.item.secondaryDisc = selectedDisciplines[1];
                                 $scope.item.secondarySubDisc = selectedDisciplines[1].subdisciplines;
