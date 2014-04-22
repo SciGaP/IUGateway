65b34952e9d0e2104a96645bae8bcb17e4552d0c
Chathuri Wimalasena <kamalasini@gmail.com>
2014-04-22 11:22:34 -0400

    spring scope

--- sda/src/main/java/org/scigap/iucig/controller/FileManagerController.java
+++ sda/src/main/java/org/scigap/iucig/controller/FileManagerController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@Scope("session")
+//@Scope("session")
 @RequestMapping(value = "/filemanager/")
 public class FileManagerController {
 
