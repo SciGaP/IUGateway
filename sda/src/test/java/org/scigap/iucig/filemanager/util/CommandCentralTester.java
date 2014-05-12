9687721fa887fa6b73c2831e7f3ce073a4ae91a4
Sachith Withana <swsachith@gmail.com>
2014-05-12 01:59:29 -0700

    adding file download tester

--- sda/src/test/java/org/scigap/iucig/filemanager/util/CommandCentralTester.java
+++ sda/src/test/java/org/scigap/iucig/filemanager/util/CommandCentralTester.java
@@ -1,6 +1,7 @@
 package org.scigap.iucig.filemanager.util;
 
 import com.jcraft.jsch.Session;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.scigap.iucig.filemanager.KerberosConnector;
@@ -25,10 +26,13 @@ public class CommandCentralTester {
     public void testDownloadFile() throws Exception {
         Session session = connector.getSession(USER);
         InputStream is = commandCentral.scpFrom(session,"/home/swithana/test.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String line = null;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
-        }
+        }*/
+
+        String myString = IOUtils.toString(is, "UTF-8");
+        System.out.println(myString);
     }
 }
