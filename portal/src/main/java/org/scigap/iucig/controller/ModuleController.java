a544f9db9637a70f93b69c070d4d4e6dcf02889b
marpierc <marpierc@iu.edu>
2017-08-09 15:34:33 -0400

    Adding carbonate

--- portal/src/main/java/org/scigap/iucig/controller/ModuleController.java
+++ portal/src/main/java/org/scigap/iucig/controller/ModuleController.java
@@ -16,54 +16,55 @@ import java.util.List;
 @Controller
 @RequestMapping(value="/modules/")
 public class ModuleController {
-	
-	@Autowired
+    
+    @Autowired
     UserService userService;
-	
-	@Autowired
+    
+    @Autowired
     ModuleService moduleService;
-	
-	private final Logger logger = Logger.getLogger(getClass());
-	
-	/** Returns the module software available on all systems */
-	@ResponseBody
-	@RequestMapping(value="/all", method = RequestMethod.GET)
-	public List<Module> getAllModuleNames() {
-		logger.debug("Getting all modules info");
-		return moduleService.getAllModuleNames();
-	}
-	
-	@ResponseBody
-	@RequestMapping(value="/{moduleName}", method = RequestMethod.GET)
-	public List<Module> getModuleDetails(@PathVariable String moduleName) {
-		logger.debug("Getting all modules info");
-		return moduleService.getModuleDetails(moduleName);
-	}
-	
-	//** Returns the module software available on Quarry *//*
-	@ResponseBody
-	@RequestMapping(value="/karst", method = RequestMethod.GET)
-	public List<Module> getKarstModules() {
-		logger.debug("Getting karst modules info");
-		return moduleService.getQuarryModules();
-	}
-
+    
+    private final Logger logger = Logger.getLogger(getClass());
+    
+    /** Returns the module software available on all systems */
+    @ResponseBody
+    @RequestMapping(value="/all", method = RequestMethod.GET)
+    public List<Module> getAllModuleNames() {
+	logger.debug("Getting all modules info");
+	return moduleService.getAllModuleNames();
+    }
+    
+    @ResponseBody
+    @RequestMapping(value="/{moduleName}", method = RequestMethod.GET)
+    public List<Module> getModuleDetails(@PathVariable String moduleName) {
+	logger.debug("Getting all modules info");
+	return moduleService.getModuleDetails(moduleName);
+    }
+    
+    //** Returns the module software available on Quarry *//*
+    @ResponseBody
+    @RequestMapping(value="/karst", method = RequestMethod.GET)
+    public List<Module> getKarstModules() {
+	logger.debug("Getting karst modules info");
+	//	return moduleService.getQuarryModules();
+	return moduleService.getKarstModules();
+    }
+    
     @ResponseBody
     @RequestMapping(value="/quarry", method = RequestMethod.GET)
     public List<Module> getQuarryModules() {
         logger.debug("Getting Quarry modules info");
         return moduleService.getQuarryModules();
     }
-
-	//** Returns the module software available on Mason *//*
-	@ResponseBody
-	@RequestMapping(value="/mason", method = RequestMethod.GET)
-	public List<Module> getMasonModules() {
-		logger.debug("Getting mason modules info");
-		return moduleService.getMasonModules();
-	}
-
+    
     //** Returns the module software available on Mason *//*
+    @ResponseBody
+    @RequestMapping(value="/mason", method = RequestMethod.GET)
+    public List<Module> getMasonModules() {
+	logger.debug("Getting mason modules info");
+	return moduleService.getMasonModules();
+    }
+    
+    //** Returns the module software available on BR2 *//*
     @ResponseBody
     @RequestMapping(value="/bigred2", method = RequestMethod.GET)
     public List<Module> getBR2Modules() {
@@ -71,15 +72,11 @@ public class ModuleController {
         return moduleService.getBR2Modules();
     }
 
-//    @ResponseBody
-//    @RequestMapping(value="/search", method = RequestMethod.GET)
-//    public List<Module> searchModules(@RequestParam(value="machine", required=false) String machine,
-//    		@RequestParam(value="name", required=false) String name,
-//    		@RequestParam(value="version", required=false) String version,
-//    		@RequestParam(value="category", required=false) String category,
-//    		@RequestParam(value="description", required=false) String description) {
-//        logger.debug("Searching modules info");
-//        return moduleService.searchModules(machine,name,version,category,description);
-//    }
+    @ResponseBody
+    @RequestMapping(value="/carbonate", method = RequestMethod.GET)
+    public List<Module> getCarbonateModules() {
+        logger.debug("Getting BR2 modules info");
+        return moduleService.getBR2Modules();
+    }
 
 }
